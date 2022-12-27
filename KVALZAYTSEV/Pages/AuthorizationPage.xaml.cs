ae8c695e6ec192272c870183bc2e3e466c3e84b8
exam_2 <exam_2@DESKTOP-PM6EI31>
2022-12-27 14:46:01 +0300

    Добавьте файлы проекта.

---
+++ KVALZAYTSEV/Pages/AuthorizationPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KVALZAYTSEV.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+        private void BEnter_click(object sender, RoutedEventArgs e)
+        {
+            Model.User user = MainWindow.db.User.FirstOrDefault(c => c.Login == TBLogin.Text && c.Password == PBPassword.Password);
+
+            if (user != null)
+            {
+                MessageBox.Show("Неопознанный пользователь");
+                return;
+            }
+            MainWindow.loggedUser = user;
+            MessageBox.Show("Успешный вход!");
+            NavigationService.Navigate(new Pages.MainMenuPage());
+
+        }
+
+        
+    }
+    
+}
